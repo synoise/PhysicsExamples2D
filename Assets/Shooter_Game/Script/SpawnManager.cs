5f3ae01f60d821d21120ab980ef5b487d4698cd2
Rio <rio.imam@gmail.com>
2023-11-15 20:12:17 +0100

    First Commit

---
+++ Assets/Shooter_Game/Script/SpawnManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    [SerializeField] Transform[] spawners;
+    [SerializeField] float interval = 5f;
+    [SerializeField] GameObject enemyPrefab;
+
+    private bool isGameStart = true;
+
+    private void Start()
+    {
+        StartCoroutine(InstantiateEnemy());
+    }
+
+    private IEnumerator InstantiateEnemy()
+    {
+        while (isGameStart)
+        {
+            yield return new WaitForSeconds(interval);
+
+            int _spawnIndex = Random.Range(0, spawners.Length);
+            Instantiate(enemyPrefab, spawners[_spawnIndex]);
+        }
+    }
+}
