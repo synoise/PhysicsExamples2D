09bb20c277227ac6911829caa6e5626b857626b7
synoise <psynos@wsiz.edu.pl>
2023-11-28 10:10:51 +0100

    Unit tests examples - TDD;

---
+++ Assets/AssetsTests.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using NUnit.Framework;
+
+using System;using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace theGame
+{
+    public class AssetsTests
+    {
+
+        public Pacman testingObj;
+        [Test]
+        public void AssetsTestsSimplePasses()
+        {
+            testingObj = new Pacman();
+            
+            Assert.IsNull(testingObj);
+
+
+
+        }
+
+        // A UnityTest behaves like a coroutine in PlayMode
+        // and allows you to yield null to skip a frame in EditMode
+        [Test]
+        public void AssetsTestsWithEnumeratorPasses()
+        {
+            testingObj = new Pacman();
+             var x = testingObj.calculate(2, 2);
+            Assert.AreEqual(4,x);
+
+        }
+    }
+}
---
+++ Assets/Pacman.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace theGame
+{
+    public class Pacman : MonoBehaviour
+    {
+        public GameObject player;
+
+        public int calculate(int a, int b)
+        {
+            var c = a * b;//+ power(a+1);
+            return c;
+        }
+
+        private int power(int a)
+        {
+            return a * a;
+        }
+    }
+    
+    
+}
