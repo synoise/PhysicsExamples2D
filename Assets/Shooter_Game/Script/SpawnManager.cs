8a8d92d4b5b4283767f1b8c63df10d1213bd260c
Rio <rio.imam@gmail.com>
2023-12-05 12:59:23 +0100

    Random spawn outside radius

--- Assets/Shooter_Game/Script/SpawnManager.cs
+++ Assets/Shooter_Game/Script/SpawnManager.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    [SerializeField] Transform[] spawners;
+    [SerializeField] Transform player;
     [SerializeField] float interval = 5f;
     [SerializeField] GameObject enemyPrefab;
+
+    [SerializeField] float maxSpawnRadius, minSpawnRadius;       // area where enemy can spawn outside radius
 
     private bool isGameStart = true;
 
@@ -21,8 +23,29 @@ public class SpawnManager : MonoBehaviour
         {
             yield return new WaitForSeconds(interval);
             interval -= 0.0001f;
-            int _spawnIndex = Random.Range(0, spawners.Length);
-            Instantiate(enemyPrefab, spawners[_spawnIndex].transform);
+
+            Vector3 randomPos = RandomizePosition();
+            Instantiate(enemyPrefab, randomPos, Quaternion.identity);
         }
+    }
+
+    private Vector3 RandomizePosition()
+    {
+        Vector3 randomPos;
+        randomPos.y = enemyPrefab.transform.position.y;
+
+        while (true)
+        {
+            randomPos.x = Random.Range(player.position.x - maxSpawnRadius, player.position.x + maxSpawnRadius);
+            randomPos.z = Random.Range(player.position.z - maxSpawnRadius, player.position.z + maxSpawnRadius);
+
+            if (Vector3.Distance(randomPos, player.position) > minSpawnRadius)
+            {
+                break;
+            }
+        }
+
+        return randomPos;
+
     }
 }
