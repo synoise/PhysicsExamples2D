12cad16b113f3dd5e8987601f816bdba0b31c457
Rio <rio.imam@gmail.com>
2023-12-05 14:56:09 +0100

    Enemy Knockback Effect

--- Assets/Shooter_Game/Script/Enemy.cs
+++ Assets/Shooter_Game/Script/Enemy.cs
@@ -8,11 +8,12 @@ using UnityEngine.UI;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] float moveSpeed;
+    
     private bool isChasing;
+    private int health = 3;
 
     private Transform target;
-    
-    [SerializeField] private TMP_Text score;
+    [SerializeField] Animator animator;
     
     void Start()
     {
@@ -40,10 +41,23 @@ public class Enemy : MonoBehaviour
         float rotationY = Mathf.Atan2(difference.x, difference.z) * Mathf.Rad2Deg ;
         transform.rotation = Quaternion.Euler(0.0f, rotationY, 0.0f);
     }
-    private void OnHit()
+    private IEnumerator OnHit()
     {
-        EventsManager.OnEnemyDied();
-        Destroy(this.gameObject);
+        health--;
+        animator.SetTrigger("Hit");
+        isChasing = false;
+
+        yield return new WaitForSeconds(0.2f);
+
+        if (health <= 0)
+        {
+            EventsManager.OnEnemyDied();
+            Destroy(gameObject);
+        }
+
+        isChasing = true;
+        StartCoroutine(Chase());
+
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -51,7 +65,7 @@ public class Enemy : MonoBehaviour
         Debug.Log(collision);
         if (collision.gameObject.CompareTag("Bullet"))
         {
-            OnHit();
+            StartCoroutine(OnHit());
             //score.text = (int.Parse(score.text) + 1).ToString();
         }
     }
