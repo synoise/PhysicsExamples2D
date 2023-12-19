cd5cc337a48c4407d3acb013ff1af4ce8dc415e2
Rio <rio.imam@gmail.com>
2023-12-19 13:18:33 +0100

    Sparks vfx

--- Assets/Shooter_Game/Script/bullet.cs
+++ Assets/Shooter_Game/Script/bullet.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.VFX;
 
 namespace Asesino_Zombie
 {
     public class Bullet : MonoBehaviour
     {
+        [SerializeField] private GameObject sparkVFXGameObject;
+
         // Start is called before the first frame update
         void Start()
         {
@@ -18,10 +21,20 @@ namespace Asesino_Zombie
             Destroy(gameObject);
         }
 
+        private Vector3 GetBulletPosition()
+        {
+            return this.transform.position;
+        }
+
+
+
         private void OnCollisionEnter(Collision collision)
         {
             if (collision.gameObject.CompareTag("Enemy"))
             {
+                Vector3 bulletLastPosition = GetBulletPosition();
+                Instantiate(sparkVFXGameObject, bulletLastPosition, collision.transform.rotation);
+
                 Destroy(gameObject);
             }
         }
